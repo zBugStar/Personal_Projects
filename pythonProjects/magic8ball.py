dda3e197ea944a5ea08af52fbdc96358d065f340
zBugStar <juan.orozco45208@ucaldas.edu.co>
2024-04-11 17:14:53 -0500

    Add new projects

---
+++ pythonProjects/magic8ball.py
@@ -0,0 +1,27 @@
+import random
+
+print("Magic 8 Ball")
+question = input("Ask me a question: ")
+answer = random.randint(1, 9)
+
+if answer == 1:
+    print("Yes - definitely.")
+elif answer == 2:
+    print("It is decidedly so.")
+elif answer == 3:
+    print("Without a doubt.")
+elif answer == 4:
+    print("Reply hazy, try again.")
+elif answer == 5:
+    print("Ask again later.")
+elif answer == 6:
+    print("Better not tell you now.")
+elif answer == 7:
+    print("My sources say no.")
+elif answer == 8:
+    print("Outlook not so good.")
+elif answer == 9:
+    print("Very doubtful.")
+
+print("Thank you for playing!")
+
