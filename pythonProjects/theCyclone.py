7e847f76e46ef7015f0f010b482bb04e83c0c5e1
zBugStar <juan.orozco45208@ucaldas.edu.co>
2024-04-15 10:43:22 -0500

    Add new projects

---
+++ pythonProjects/theCyclone.py
@@ -0,0 +1,20 @@
+print("Let's see if you meet the requirements to enter the cyclone")
+
+height = int(input("Enter your height in CM: "))
+credits = int(input("Enter the number of credits you have: "))
+message = ""
+
+if height < 132 and credits < 10:
+    message = "Sorry, you are too short and do not have enough credits to ride the cyclone"
+elif credits < 10:
+    message = "Sorry, you do not have enough credits to ride the cyclone"
+elif height < 132:
+    message = "Sorry, you are too short to ride the cyclone"
+
+if height >= 132 and credits >= 10:
+    message = "You can ride the cyclone!"
+
+print(message)
+
+
+
