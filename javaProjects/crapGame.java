2f09b865ca948006c50ca02ab7e524b57135b690
zBugStar <juan.orozco45208@ucaldas.edu.co>
2024-04-16 10:11:39 -0500

    Add new project

--- javaProjects/crapGame.java
+++ javaProjects/crapGame.java
@@ -1,42 +1,72 @@
 import java.util.Random;
 
 public class crapGame {
-    public static void main(String[] args) {
-        Random random = new Random();
-        int roll = rollDice(random);
-        int point = 0;
-        boolean gameOn = true;
 
-        System.out.println("Welcome to the Craps Game!");
+    public static void main(String[] args){
 
-        if (roll == 7 || roll == 11) {
-            System.out.println("You rolled " + roll + ". You win!");
-            gameOn = false;
-        } else if (roll == 2 || roll == 3 || roll == 12) {
-            System.out.println("You rolled " + roll + ". You lose!");
-            gameOn = false;
-        } else {
-            point = roll;
-            System.out.println("You rolled " + roll + ". Point is set to " + point);
-        }
+        Random rnd = new Random();
+        boolean continuar = true;
+        int punto = 0;
+        
+         System.out.println("----Vamos a simular el lanzamiento de unos dados----");
 
-        while (gameOn) {
-            roll = rollDice(random);
-            if (roll == point) {
-                System.out.println("You rolled " + roll + ". You win!");
-                gameOn = false;
-            } else if (roll == 7) {
-                System.out.println("You rolled " + roll + ". You lose!");
-                gameOn = false;
-            } else {
-                System.out.println("You rolled " + roll + ". Roll again.");
+         int puntaje = tirarDados(rnd);
+         System.out.println("--- Sacaste "+ puntaje+ " ---");
+
+         if (puntaje == 7 || puntaje == 11) {
+
+             System.out.println("Ganaste en la primera tirada");
+             System.out.println("-----------------------------");
+             continuar = false;
+
+          }
+         else if (puntaje == 2 || puntaje == 3 || puntaje == 12) {
+
+             System.out.println("Perdiste ");
+             System.out.println("-----------------------------");
+             continuar = false;
+
+         }
+         else{
+             punto = puntaje;
+             System.out.println("Sigues lanzando, tu punto es "+ punto);
+             System.out.println("-----------------------------");
+
+         }
+
+
+         while (continuar) {
+
+             puntaje = tirarDados(rnd);
+
+             if (puntaje == punto) {
+
+                 System.out.println("Sacaste "+ puntaje +" Ganaste!");
+                 System.out.println("-----------------------------");
+                 continuar = false;
+
             }
-        }
+             else if (puntaje == 7) {
+
+                System.out.println("Sacaste "+ puntaje +" Perdiste!");
+                System.out.println("-----------------------------");
+                continuar = false;
+
+            }
+             else {
+
+                System.out.println("Sacaste "+ puntaje +" Sigues lanzando.");
+                System.out.println("-----------------------------");
+
+            }
+         }
     }
 
-    private static int rollDice(Random random) {
-        int die1 = random.nextInt(6) + 1;
-        int die2 = random.nextInt(6) + 1;
-        return die1 + die2;
+
+    public static int tirarDados(Random rnd){
+
+        int dado1 = rnd.nextInt(6)+1;
+        int dado2 = rnd.nextInt(6)+1;
+        return dado1+dado2;
     }
 }
