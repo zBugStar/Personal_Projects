21b6c0caa847f0a5a5e405581f0f060f96cf221c
zBugStar <juan.orozco45208@ucaldas.edu.co>
2024-04-18 10:54:48 -0500

    add new projects

---
+++ pythonProjects/99Bottles.py
@@ -0,0 +1,6 @@
+for i in range(99, 0, -1):
+    print(f"{i} bottles of beer on the wall")
+    print(f"{i} bottles of beer")
+    print(f"Take one down, pass it around,")
+    print(f"{i-1} bottles of beer on the wall")
+    print()
