673de5310410c234f350e21b1ebd8013ffc238ab
zBugStar <juan.orozco45208@ucaldas.edu.co>
2024-04-23 17:04:02 -0500

    New changes

--- pythonProjects/POO/interfaces.py
+++ pythonProjects/POO/interfaces.py
@@ -3,30 +3,30 @@ from tkinter import *
 
 raiz = Tk()
 raiz.title("Ventana de pruebas")
-raiz.resizable(1, 1)
-raiz.geometry("500x500")
-raiz.resizable(0, 0)
+raiz.geometry("960x540")
+raiz.resizable(1,1)
 raiz.config(bg="white")
 
 label1 = tkinter.Label(raiz, text="Hola, bienvenido a mi programa", bg="lightblue", fg="beige", font=("Arial", 12))
 label1.place(x=100, y=100)
 
+
 def saludo():
-    tkinter.Label(raiz, text="Hola, bienvenido a mi programa", bg="lightblue", fg="beige", font=("Arial", 12)).pack()
+    tkinter.Label(raiz, text="Hola, bienvenido a mi programa", bg="lightblue", fg="beige", font=("Arial", 12), )
 
 
 def salir():
     raiz.destroy()
 
 
-button = tkinter.Button(raiz, text="Salir", command=salir)
+button = tkinter.Button(raiz, text="Salir", height=5, width=10, command=salir)
 button.config(bg="red", fg="white", font=("Arial", 12))
-button.place(x=250, y=250)
+button.winfo_geometry()
+button.place(x=250, y=338.9)
 
-button2 = tkinter.Button(raiz, text="Saludo", command=saludo)
-button2.config(bg="green", fg="white", font=("Arial", 12))
-button2.place(x=250, y=100, width=100, height=50)
-
+button2 = tkinter.Button(raiz, text="Saludo", height=5, width=10, command=saludo,)
+button2.config(bg="green", fg="white", font=("Courier", 12))
+button2.place(x=250, y=100,)
 
 raiz.mainloop()
 
