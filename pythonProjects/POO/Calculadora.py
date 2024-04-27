87a80e2e5c410ad26dc1817f8e2daa1ed8999722
zBugStar <juan.orozco45208@ucaldas.edu.co>
2024-04-26 23:09:21 -0500

    New project

---
+++ pythonProjects/POO/Calculadora.py
@@ -0,0 +1,15 @@
+import tkinter
+from tkinter import *
+
+window = Tk()
+window.title("Calculator")
+window.geometry("400x500")
+window.resizable(False, False)
+window.config(bg="light grey")
+
+
+
+
+
+
+window.mainloop()
